c1325304d1c80281b7f0a01a621c7001447abd76
Gene Liverman <gliverma@westga.edu>
2015-04-12 00:36:14 -0400

    Added some pages

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,10 +8,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/hello/{name}.{_format}",
+     *     defaults = {"_format"="html"},
+     *     requirements = { "_format" = "html|xml|json" },
+     *     name = "hello"
+     * )
+     */
+    public function helloAction($name, $_format)
+    {
+        return $this->render('default/hello.'.$_format.'.twig', array(
+            'name' => $name
+        ));
     }
 }
